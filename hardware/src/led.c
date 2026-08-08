8820ddd66f5c5b51fbd64a28801a8268044cc013
Larry787866 <2019103827@qq.com>
2026-08-08 10:07:48 +0800

    题目1改

--- hardware/src/led.c
+++ hardware/src/led.c
@@ -7,7 +7,7 @@
 static const uint16_t led_pins[] = {LED1_PIN, LED2_PIN, LED3_PIN, LED4_PIN};
 
 #define IS_VALID_LED(id) ((id) < LED_COUNT)
-#define LED_COUNT 4
+#define LED_COUNT 4U
 /* 点亮 LED1 */
 void led_on(uint8_t led_num)
 {
@@ -15,9 +15,6 @@ void led_on(uint8_t led_num)
         return; 
     }
 
-    if (led_num >= LED_COUNT) {
-        return; 
-    }
     HAL_GPIO_WritePin(LED_GPIO_PORT, led_pins[led_num], GPIO_PIN_SET);
 }
 
@@ -28,8 +25,5 @@ void led_off(uint8_t led_num)
         return; 
     }
 
-    if (led_num >= LED_COUNT) {
-        return; 
-    }
     HAL_GPIO_WritePin(LED_GPIO_PORT, led_pins[led_num], GPIO_PIN_RESET);
 }
