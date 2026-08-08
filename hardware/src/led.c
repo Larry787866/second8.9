cc88131685ce73db15c7c84578b69d052c387cd9
Larry787866 <2019103827@qq.com>
2026-08-08 09:58:33 +0800

    作业1

--- hardware/src/led.c
+++ hardware/src/led.c
@@ -4,14 +4,32 @@
  */
 #include "led.h"
 
+static const uint16_t led_pins[] = {LED1_PIN, LED2_PIN, LED3_PIN, LED4_PIN};
+
+#define IS_VALID_LED(id) ((id) < LED_COUNT)
+#define LED_COUNT 4
 /* 点亮 LED1 */
-void led_on(void)
+void led_on(uint8_t led_num)
 {
-    HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN, GPIO_PIN_SET);
+    if (!IS_VALID_LED(led_num)) {
+        return; 
+    }
+
+    if (led_num >= LED_COUNT) {
+        return; 
+    }
+    HAL_GPIO_WritePin(LED_GPIO_PORT, led_pins[led_num], GPIO_PIN_SET);
 }
 
 /* 熄灭 LED1 */
-void led_off(void)
-{
-    HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN, GPIO_PIN_RESET);
+void led_off(uint8_t led_num)
+{    
+    if (!IS_VALID_LED(led_num)) {
+        return; 
+    }
+
+    if (led_num >= LED_COUNT) {
+        return; 
+    }
+    HAL_GPIO_WritePin(LED_GPIO_PORT, led_pins[led_num], GPIO_PIN_RESET);
 }
